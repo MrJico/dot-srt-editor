ba6c3728e51112b829e67b06379bf18d4dd1bbd1
Roj Serbest <rojserbest@gmail.com>
2021-05-04 09:29:47 +0300

    Add /new command

---
+++ handlers/new.py
@@ -0,0 +1,39 @@
+import pysrt
+import pysrt.srttime
+import pysrt.srtitem
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+
+from helpers import decorators
+from helpers import send
+from strings import get
+
+
+@decorators.file
+def new(update: Update, context: CallbackContext):
+    args = update.effective_message.text.split()
+    if len(args) != 3:
+        update.effective_message.reply_text(get('usage_of_new_command'))
+        return
+    try:
+        start = pysrt.srttime.SubRipTime.from_string(args[1])
+        end = pysrt.srttime.SubRipTime.from_string(args[2])
+    except pysrt.srttime.InvalidTimeString:
+        update.effective_message.reply_text(get('invalid_time_provided'))
+        return
+    file = pysrt.open(context.chat_data['file'], encoding='UTF-8')
+    file.append(
+        pysrt.srtitem.SubRipItem(
+            start=pysrt.srttime.SubRipTime.from_string(start),
+            end=pysrt.srttime.SubRipTime.from_string(end)
+        )
+    )
+    file.clean_indexes()
+    file.save(context.chat_data['file'], encoding='UTF-8')
+    context.chat_data['line'] = 0
+    update.effective_message.reply_text(get('line_added'))
+    send.current_line(update, context)
+
+
+handlers = [[CommandHandler('new', new)]]
