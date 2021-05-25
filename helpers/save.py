a3dbcc50edf98f8cd20c6af1d106718388935b1d
MrJico <hawar4bug@gmail.com>
2021-05-25 09:18:03 +0300

    added hex color code support

--- helpers/save.py
+++ helpers/save.py
@@ -4,11 +4,10 @@ from html import escape
 
 def get_to_save(string: str):
     match_list = re.findall(
-        r"(<#([aA-zZ]+)#(\s*[\w\s]+\s*)>)", string, flags=re.UNICODE
+        r"(<#([aA-zZ]+|[aA-fF0-9]{6})#(\s*[\w\s]+\s*)>)", string, flags=re.UNICODE
     )
     if match_list:
         for match in match_list:
-            print(match)
             color = match[1]
             text = escape(match[2].strip())
             colored = f'<span style="color: #{color};">{text}</span>'
